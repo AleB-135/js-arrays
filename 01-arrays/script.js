48da82116adda3a46d00be95ae79a2781a4fcfaf
AleB-135 <alessiobrusca1999@outlook.com>
2025-02-23 17:27:57 +0100

    feat: Svolto esercizio 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -21,11 +21,11 @@ const fourthTeacher = teachers.at(3);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
-
+const lastTeacher = "Luca";
+    teachers.pop();
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
-
+const firstTeacher = "Nathan";
+    teachers.shift();
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
